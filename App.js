cc0b16b7833eb023b9fa515a67fffb93b05d9ece
jakub.vano <jakub.vano@accenture.com>
2023-01-25 15:16:55 +0100

    SIMON - colors, styling and array to object

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { Colors } from './assets/colors';
+import { ColorSet } from './assets/colorSet';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { useState } from 'react';
 import {
@@ -11,9 +11,7 @@ import {
   Pressable,
 } from 'react-native';
 
-const today = undefined;
 const datesCount = 29;
-const currentMonth = moment().month();
 
 export default function App() {
   const [dates, setDates] = useState(getInitialDates());
@@ -22,17 +20,17 @@ export default function App() {
 
   function getInitialDates() {
     return getDates(
-      today,
+      undefined,
       datesCount - 1,
-      ['...', undefined, Colors.grey],
-      ['DEL', moment().subtract(1, 'days'), Colors.grey]
+      { label: '...', date: null, style: styles.LastItem },
+      { label: 'Del', date: moment().subtract(1, 'days'), style: styles.DeleteItem }
     );
   }
 
   function getMonthColor(date) {
-    return (date.month() - currentMonth) % 2
-      ? Colors.darkerRed
-      : Colors.darkRed;
+    return (date.month() - moment().month()) % 2
+      ? styles.OddMonth
+      : styles.EvenMonth;
   }
 
   function getDates(start, count, lastElementLabel, firstItem = null) {
@@ -40,7 +38,11 @@ export default function App() {
 
     for (let i = 0; i < count; i++) {
       const date = moment(start).add(i, 'days');
-      ret.push([date.format('DD.M.'), date, getMonthColor(date)]);
+      ret.push({
+        label: date.format('DD.M.'),
+        date: date,
+        style: getMonthColor(date),
+      });
     }
 
     ret.push(lastElementLabel);
@@ -48,28 +50,22 @@ export default function App() {
   }
 
   function GetGridViewItem(item) {
-    if (item[0] === '...') {
+    if (item.label === '...') {
       setDates(
-        getDates(moment(today).add(datesCount - 1, 'days'), datesCount, [
-          '?',
-          undefined,
-          Colors.grey,
-        ])
+        getDates(moment().add(datesCount - 1, 'days'), datesCount, {
+          label: '?',
+          date: undefined,
+          style: styles.LastItem,
+        })
       );
       return;
     }
 
-    if (item[0] === '?') {
+    if (item.label === '?') {
       setIsJustInput(true);
-      // DateTimePickerAndroid.open({
-      //   value: this.state.dateValue,
-      //   onChange: ((_date) => {this.setState({dateValue: _date})}),
-      //   mode: 'date',
-      // });
       return;
     }
-    setDateValue(item[1]);
-    // Alert.alert(item[0]);
+    setDateValue(item?.date);
   }
 
   if (dateValue !== undefined) {
@@ -92,25 +88,27 @@ export default function App() {
   } else {
     if (!isJustInput) {
       return (
-        <View style={{ flex: 1 }}>
+        <View style={styles.MainWrapper}>
           <Text style={styles.Item}>Item: Fresh Milk 1L</Text>
           <Text style={styles.Barcode}>[ 316543154983125 ]</Text>
           <View style={styles.MainContainer}>
             <FlatList
               data={dates}
-              renderItem={({ item }) => (
-                <View
-                  style={[
-                    styles.GridViewBlockStyle,
-                    { backgroundColor: item[2] },
-                  ]}>
-                  <Text
-                    style={styles.GridViewInsideTextItemStyle}
-                    onPress={() => GetGridViewItem(item)}>
-                    {item[0]}
-                  </Text>
-                </View>
-              )}
+              renderItem={({ item }) => {
+                return (
+                  <View
+                    style={[
+                      styles.GridViewBlockStyle,
+                      { backgroundColor: item?.style?.backgroundColor },
+                    ]}>
+                    <Text
+                      style={styles.GridViewInsideTextItemStyle}
+                      onPress={() => GetGridViewItem(item)}>
+                      {item.label}
+                    </Text>
+                  </View>
+                );
+              }}
               numColumns={5}
             />
           </View>
@@ -125,7 +123,7 @@ export default function App() {
             testID="dateTimePicker"
             value={new Date(moment().add(datesCount * 2))}
             mode={'date'}
-            color={Colors.darkRed}
+            color={Colors.dateTimePickerColor}
             onChange={(_date) => {
               setDateValue(new Date(_date.nativeEvent.timestamp));
             }}
@@ -137,6 +135,9 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  MainWrapper: {
+    flex: 1,
+  },
   MainContainer: {
     justifyContent: 'center',
     position: 'absolute',
@@ -154,11 +155,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: 50,
     margin: 3,
-    backgroundColor: Colors.dimgray,
     borderRadius: 10,
   },
   GridViewInsideTextItemStyle: {
-    color: Colors.white,
+    color: ColorSet.gridTextColor,
     padding: 5,
     fontSize: 14,
     fontWeight: 'bold',
@@ -166,21 +166,21 @@ const styles = StyleSheet.create({
   },
   Item: {
     marginTop: 20,
-    color: Colors.black,
+    color: ColorSet.itemColor,
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center', // <-- the magic
   },
   Barcode: {
     marginVertical: 10,
-    color: Colors.black,
+    color: ColorSet.barcode,
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center', // <-- the magic
   },
   DateValue: {
     marginTop: 250,
-    color: Colors.black,
+    color: ColorSet.dateValueColor,
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center', // <-- the magic
@@ -192,7 +192,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: Colors.darkRed,
+    backgroundColor: ColorSet.resetButtonBackground,
     margin: 30,
   },
   ResetText: {
@@ -200,6 +200,18 @@ const styles = StyleSheet.create({
     lineHeight: 21,
     fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: Colors.white,
+    color: ColorSet.resetButtonText,
+  },
+  LastItem: {
+    backgroundColor: ColorSet.lastItemBackground,
+  },
+  DeleteItem: {
+    backgroundColor: ColorSet.deleteItemBackground,
+  },
+  OddMonth: {
+    backgroundColor: ColorSet.oddMonth,
+  },
+  EvenMonth: {
+    backgroundColor: ColorSet.evenMonth,
   },
 });
---
+++ assets/colorSet.js
@@ -0,0 +1,24 @@
+import { Colors } from './colors';
+
+export const ColorSet = {
+  // scanned item
+  itemColor: Colors.black,
+  barcode: Colors.black,
+
+  // grid
+  gridTextColor: Colors.white,
+  oddMonth: Colors.darkerRed,
+  evenMonth: Colors.darkRed,
+  lastItemBackground: Colors.grey,
+  deleteItemBackground: Colors.grey,
+
+  // reset button
+  resetButtonBackground: Colors.darkerRed,
+  resetButtonText: Colors.white,
+
+  // selected date
+  dateValueColor: Colors.black,
+
+  // native date picker
+  dateTimePickerColor: Colors.darkRed,
+};
